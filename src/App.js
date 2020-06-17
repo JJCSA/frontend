e8f9d22c0113ee5489c1a95d4099a1396731abff
Tejas Shah <shah.te@northeastern.edu>
2020-06-16 22:28:11 -0400

    Add react-bootstrap-table-next

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 import Navbar from './components/Navbar'
 import Landing from './components/Landing'
--- src/pages/admin/UserManager.js
+++ src/pages/admin/UserManager.js
@@ -1,24 +1,56 @@
 import React, { Component } from 'react';
 import DataTable from '../../components/DataTable';
 
+const users = [
+    {
+        "user_id": 1,
+        "image": "img",
+        "name": "Tejas Shah",
+        "type": "Admin",
+        "email": "a@a.com",
+        "number": "8573139750",
+        "status": "Pending",
+        "state": "Massachusetts",
+        "city": "Boston"
+    },
+    {
+        "user_id": 2,
+        "image": "img",
+        "name": "Alex Beck",
+        "type": "User",
+        "email": "b@a.com",
+        "number": "8573139750",
+        "status": "Pending",
+        "state": "Massachusetts",
+        "city": "Boston"
+    }
+];
+
 class UserManager extends Component {
 
-    
+    getColumnHeadings = () => {
+        const columns = [];
+        const obj = users[0];
+        for(const objKey in obj) {
+            columns.push({
+                dataField: objKey,
+                text: objKey,
+                hidden: (objKey.includes("_id"))
+            });
+        }
+
+        return columns;
+    }
 
     render() {
-        const users = [
-            {
-                "user_id": 1,
-                "image": "img",
-                "firstName": "Tejas",
-                "lastName": "Shah",
-                "email": "a@a.com"
-            }
-        ];
         return (
             <div>
                 Users
-                <DataTable users={users} />
+                <DataTable 
+                    keyField="user_id"
+                    columns={this.getColumnHeadings()} 
+                    data={users} 
+                />
             </div>
         )
     }
