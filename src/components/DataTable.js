bbf26686f2e4d31739e93e0f7b8d94b7e05faa9b
Tejas Shah <shah.te@northeastern.edu>
2020-06-16 22:28:11 -0400

    Add react-bootstrap-table-next

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,33 +1,15 @@
 import React, { Component } from 'react';
-import Table from 'react-bootstrap/Table';
+import BootstrapTable from 'react-bootstrap-table-next';
 
 class DataTable extends Component {
 
-    renderUser = (user) => {
-        return (
-            <tr key={user.user_id}>
-                <td>{user.image}</td>
-                <td>{user.firstName}</td>
-                <td>{user.lastName}</td>
-                <td>{user.email}</td>
-            </tr>
-        )
-    }
-
     render() {
-        const users = this.props.users;
         return(
-            <Table striped bordered hover>
-                <thead>
-                    <th>Image</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Email</th>
-                </thead>
-                <tbody>
-                    {users && users.map(this.renderUser)}
-                </tbody>
-            </Table>
+            <BootstrapTable
+                keyField={this.props.keyField}
+                data={this.props.data}
+                columns={this.props.columns}
+            />
         );
     }
 }
--- src/pages/admin/UserManager.js
+++ src/pages/admin/UserManager.js
@@ -67,6 +67,31 @@ const tableColumns = [
   },
 ];
 
+const users = [
+    {
+        "user_id": 1,
+        "image": "img",
+        "name": "Tejas Shah",
+        "type": "Admin",
+        "email": "a@a.com",
+        "number": "8573139750",
+        "status": "Pending",
+        "state": "Massachusetts",
+        "city": "Boston"
+    },
+    {
+        "user_id": 2,
+        "image": "img",
+        "name": "Alex Beck",
+        "type": "User",
+        "email": "b@a.com",
+        "number": "8573139750",
+        "status": "Pending",
+        "state": "Massachusetts",
+        "city": "Boston"
+    }
+];
+
 class UserManager extends Component {
   constructor() {
     super();
