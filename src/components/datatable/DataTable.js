fe2f13b29e066d2e642427474f076587e990296a
Tejas Shah <shah.te@northeastern.edu>
2020-06-18 20:50:28 -0400

    Add styling to data table

--- src/components/datatable/DataTable.js
+++ src/components/datatable/DataTable.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 
+import './DataTable.scss';
+
 class DataTable extends Component {
 
     render() {
+
+        const rowClasses = 'tableRow';
+
         return(
             <BootstrapTable
                 keyField={this.props.keyField}
                 data={this.props.data}
                 columns={this.props.columns}
+                bordered={false}
+                noDataIndication="Table is Empty"
+                rowClasses={rowClasses}
             />
         );
     }
