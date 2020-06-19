38771bb35628e8a3f0a0cd1e90694a41d6f232c6
Tejas Shah <shah.te@northeastern.edu>
2020-06-19 02:51:27 -0400

    Add pagination to data table

--- src/components/datatable/DataTable.js
+++ src/components/datatable/DataTable.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
 
 import './DataTable.scss';
 
 class DataTable extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { sizePerPage: 10 };
+    }
+
     render() {
+
+        const sizePerPageRenderer = ({
+            options,
+            currSizePerPage,
+            onSizePerPageChange
+          }) => (
+              <span>
+                  Show
+                    <input 
+                        type="number"
+                        min="10"
+                        max="50" 
+                        step="5"
+                        value={this.state.sizePerPage}
+                        onChange={ (e) => {
+                            this.setState({sizePerPage: e.target.value});
+                            onSizePerPageChange(e.target.value);
+                            e.preventDefault();
+                        }}
+                    />
+                  entries
+              </span>
+          );
+          
+          const options = {
+            sizePerPageRenderer
+          };
 
         const rowClasses = 'tableRow';
 
@@ -17,6 +50,7 @@ class DataTable extends Component {
                 bordered={false}
                 noDataIndication="Table is Empty"
                 rowClasses={rowClasses}
+                pagination={ paginationFactory(options) }
             />
         );
     }
