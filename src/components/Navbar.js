97aaae905aab9cd0d52105af8e83ca9826d311e2
karansheth <karan.sheth60@gmail.com>
2020-06-19 22:59:46 -0400

    Changeing Back to Log In

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ class Landing extends Component {
       <ul className="navbar-nav">
         <li className="nav-item">
           <Link to="/login" className="nav-link">
-            Sign in
+            Log In
           </Link>
         </li>
         <li className="nav-item">
