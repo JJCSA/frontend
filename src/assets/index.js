12bbce2aea6b9c7872fed3aeaa62e28c0ff63524
Divita Vora <divita.vora@gmail.com>
2020-08-29 16:35:21 -0700

    Addressing the code review

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,14 +1,12 @@
-import locationIcon from "./icons/location-icon.svg";
-import infoIcon from "./icons/info-icon.svg";
-import emailIcon from "./icons/email-icon.svg";
-import phoneIcon from "./icons/phone-icon.svg";
-import caseIcon from "./icons/case-icon.svg";
-import bookIcon from "./icons/book-icon.svg";
-import tickIcon from "./icons/tick-icon.svg";
-import communityIcon from "./icons/community-icon.svg";
-import attachmentIcon from "./icons/attachment-icon.svg";
-import universityIcon from "./icons/university-icon.svg";
-import companyIcon from "./icons/company-icon.svg";
-import deleteIcon from "./icons/delete-icon.svg"
-
-export {locationIcon, infoIcon, emailIcon, phoneIcon, caseIcon, bookIcon, tickIcon, communityIcon, attachmentIcon, universityIcon, companyIcon, deleteIcon}
+export { default as locationIcon } from './icons/location-icon.svg'
+export { default as infoIcon } from './icons/info-icon.svg'
+export { default as emailIcon } from './icons/email-icon.svg'
+export { default as phoneIcon } from './icons/phone-icon.svg'
+export { default as caseIcon } from './icons/case-icon.svg'
+export { default as bookIcon } from './icons/book-icon.svg'
+export { default as tickIcon } from './icons/tick-icon.svg'
+export { default as communityIcon } from './icons/community-icon.svg'
+export { default as attachmentIcon } from './icons/attachment-icon.svg'
+export { default as universityIcon } from './icons/university-icon.svg'
+export { default as companyIcon } from './icons/company-icon.svg'
+export { default as deleteIcon } from './icons/delete-icon.svg'
