5c96fa5ea28eecd8585464561ccd54ae60ac6b69
Ronak Shah <ronakshah2895@gmail.com>
2021-03-26 21:14:23 -0700

    completed news feed

--- src/pages/landingpage/components/newsFeed/NewsFeed.js
+++ src/pages/landingpage/components/newsFeed/NewsFeed.js
@@ -4,34 +4,78 @@ import "./NewsFeed.scss";
 class NewsFeed extends Component {
   render() {
     return (
-      <div>
+      <div class="NEWSFEED">
         <div className="text-center mt-5">
           <h4>News Feed</h4>
           <hr className="news-feed"></hr>
         </div>
-        <div className="container-fluid ">
-          <div className="row d-flex ml-5">
-            <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-              <div className=" mt-3">
-                <div className="card border-0">
-                  <div className="card-horizontal">
-                    <div className="col-lg-2 col-md-2 col-sm-12 col-xs-12">
-                      {/*<img className="" src="http://via.placeholder.com" alt="Card image cap"/>*/}
-                    </div>
-
-                    <div className="col-lg-10">
-                      <div className="card-body">
-                        <h4 className="card-title">Future events</h4>
-                        <p className="card-text ">
-                          2nd JJC SA USA Conference. August 11, 2020 marked the
-                          1st Anniversary for JJC SA USA. To commemorate this
-                          occasion there is a celebration to be held virtually
-                          on September 12th, 2020.
-                        </p>
-                      </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card news-card mb-3">
+                <div className="row no-gutters">
+                  <div className="col-md-4">
+                    <img src="https://via.placeholder.com/150" className="card-img" alt="..." />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">Food Assistance for Students during COVID-19 Pandemic.</h5>
+                      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      <p className="card-text read-more" data-toggle="modal" data-target="#newsDetailModal">Read More &gt;</p>
                     </div>
                   </div>
                 </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card news-card mb-3">
+                <div className="row no-gutters">
+                  <div className="col-md-4">
+                    <img src="https://via.placeholder.com/150" className="card-img" alt="..." />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">Food Assistance for Students during COVID-19 Pandemic.</h5>
+                      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      <p className="card-text read-more" data-toggle="modal" data-target="#newsDetailModal">Read More &gt;</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card news-card mb-3">
+                <div className="row no-gutters">
+                  <div className="col-md-4">
+                    <img src="https://via.placeholder.com/150" className="card-img" alt="..." />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">Food Assistance for Students during COVID-19 Pandemic.</h5>
+                      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      <p className="card-text read-more" data-toggle="modal" data-target="#newsDetailModal">Read More &gt;</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="modal fade" id="newsDetailModal" tabIndex="-1" role="dialog" aria-labelledby="newsDetailModalLabel" aria-hidden="true">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Food Assistance for Students during COVID-19 Pandemic.</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
               </div>
             </div>
           </div>
