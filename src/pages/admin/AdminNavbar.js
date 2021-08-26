77b93532ed072f36dbbc051fa318122a821d736c
dedhiadimpi <dedhia.di@husky.neu.edu>
2021-08-26 16:23:54 -0400

    Changed user dropdown

--- src/pages/admin/AdminNavbar.js
+++ src/pages/admin/AdminNavbar.js
@@ -6,20 +6,36 @@ import { OverlayTrigger, Popover, Badge } from 'react-bootstrap';
 import { jjcIcon, UserDropDownIcon, NotificationIcon } from '../../assets/index';
 import Avatar from '../../components/avatar/Avatar';
 import {BsBell} from 'react-icons/bs';
+import {CgProfile} from 'react-icons/cg';
+import {GoHome} from 'react-icons/go';
+import {FiLogOut} from 'react-icons/fi';
 import './AdminNavbar.scss';
 
 function AdminNavbar() {
     const signOut = useSignOut();
     const auth = useAuthUser();
 
-    const popover = (
+    const userDropdown = (
         <Popover>
             <Popover.Content className="user-profile-dropdown-content">
-                <div className="p-1" onClick={() => document.body.click()}><Link to="/">Home</Link></div>
+                <div className="user-dropdown">
+                    <GoHome className="user-dropdown-icon"/>
+                    <Link to="/">
+                        Home
+                    </Link>
+                </div>
                 <hr className="m-0" />
-                <div className="p-1" onClick={() => document.body.click()}><Link to="/profile">Profile</Link></div>
+                <div className="user-dropdown">
+                    <CgProfile className="user-dropdown-icon"/>
+                    <Link to="/profile">
+                        Profile
+                    </Link>
+                </div>
                 <hr className="m-0" />
-                <div className="p-1" onClick={signOut}>Logout</div>
+                <div className="user-dropdown">
+                    <FiLogOut className="user-dropdown-icon"/> 
+                    <span onClick={signOut}>Logout</span>
+                </div>
             </Popover.Content>
         </Popover>
     );
@@ -58,7 +74,7 @@ function AdminNavbar() {
                         Hi,
                         {auth()?.user === undefined ? 'Dude' : auth().user}
                     </h6>
-                    <OverlayTrigger trigger="click" placement="bottom" overlay={popover} rootClose>
+                    <OverlayTrigger trigger="click" placement="bottom" overlay={userDropdown} rootClose>
                         <img src={UserDropDownIcon} alt="UserDropdownIcon" className="mr-sm-5 user-profile-dropdown-icon" />
                     </OverlayTrigger>
                 </div>
--- src/pages/user/UserNavbar.js
+++ src/pages/user/UserNavbar.js
@@ -5,22 +5,38 @@ import { useAuthUser, useSignOut } from 'react-auth-kit';
 import { OverlayTrigger, Popover } from 'react-bootstrap';
 import { jjcIcon, UserDropDownIcon } from '../../assets/index';
 import Avatar from '../../components/avatar/Avatar';
+import {CgProfile} from 'react-icons/cg';
+import {GoHome} from 'react-icons/go';
+import {FiLogOut} from 'react-icons/fi';
 import './UserNavbar.scss';
 
 function UserNavbar() {
   const signOut = useSignOut();
   const auth = useAuthUser();
 
-  const popover = (
+  const userDropdown = (
     <Popover>
-      <Popover.Content className="user-profile-dropdown-content">
-        <div className="p-1" onClick={() => document.body.click()}><Link to="/admin">Admin</Link></div>
-        <hr className="m-0" />
-        <div className="p-1" onClick={() => document.body.click()}><Link to="/profile">Profile</Link></div>
-        <hr className="m-0" />
-        <div className="p-1" onClick={signOut}>Logout</div>
-      </Popover.Content>
-    </Popover>
+            <Popover.Content className="user-profile-dropdown-content">
+                <div className="user-dropdown">
+                    <GoHome className="user-dropdown-icon"/>
+                    <Link to="/admin">
+                        Admin
+                    </Link>
+                </div>
+                <hr className="m-0" />
+                <div className="user-dropdown">
+                    <CgProfile className="user-dropdown-icon"/>
+                    <Link to="/profile">
+                        Profile
+                    </Link>
+                </div>
+                <hr className="m-0" />
+                <div className="user-dropdown">
+                    <FiLogOut className="user-dropdown-icon"/> 
+                    <span onClick={signOut}>Logout</span>
+                </div>
+            </Popover.Content>
+        </Popover>
   );
 
   return (
@@ -84,7 +100,7 @@ function UserNavbar() {
             Hi,
             {auth()?.user === undefined ? 'Dude' : auth().user}
           </h6>
-          <OverlayTrigger trigger="click" placement="bottom" overlay={popover} rootClose>
+          <OverlayTrigger trigger="click" placement="bottom" overlay={userDropdown} rootClose>
             <img src={UserDropDownIcon} alt="UserDropdownIcon" className="mr-sm-5 user-profile-dropdown-icon" />
           </OverlayTrigger>
         </form>
