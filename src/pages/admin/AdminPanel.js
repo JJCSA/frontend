8d84f67397c61af813b8879515b774a58e79f841
dedhiadimpi <dedhia.di@husky.neu.edu>
2021-08-31 15:19:26 -0400

    Changes in Admin Panel (Sidenavbar highlight color change, Added Footer)

--- src/pages/admin/AdminPanel.js
+++ src/pages/admin/AdminPanel.js
@@ -1,54 +1,26 @@
+import React, { Component, useState, useEffect } from 'react';
 import AdminHomepage from './AdminHomepage';
 import styled from 'styled-components';
 import UserManager from './UserManager';
 import { barChartIcon, userIcon, newsIcon, formIcon, feedbackIcon, socialMediaIcon, forumIcon } from "../../assets/index.js";
 import { Switch, Route, withRouter } from 'react-router-dom';
-import React, { Component } from 'react';
 import SideNav, { NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 import AdminNavbar from './AdminNavbar';
+import {BiCopyright} from 'react-icons/bi';
 import './AdminPanel.scss';
 
 const Main = styled.main`
-    position: relative;
-    //overflow: scroll;
-    transition: all .15s;
-    margin-left: ${props => (props.expanded ? 240 : 64)}px;
-`;
+        position: relative;
+        //overflow: scroll;
+        transition: all .15s;
+        margin-left: ${props => (props.expanded ? 240 : 64)}px;
+    `;
 
-class AdminPanel extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selected: 'dashboard',
-            expanded: false
-        };
-    }
+function AdminPanel(props) {
+    const [selected, setSelected] = useState('dashboard');
+    const [expanded, setExpanded] = useState(false);
 
-    componentDidMount() {
-        this.props.toggleNavbar(false);
-        this.props.toggleFooter(false);
-        this.onSelect(this.state.selected);
-    }
-
-    componentWillUnmount() {
-        this.props.toggleNavbar(true);
-        this.props.toggleFooter(true);
-    }
-
-    onSelect = (selected) => {
-        const { history } = this.props;
-        this.setState({ selected: selected });
-        const to = '/admin/' + selected;
-        if (history.location.pathname !== to) {
-            history.push(to);
-        }
-    };
-
-    onToggle = (expanded) => {
-        this.setState({ expanded: expanded });
-    };
-
-    renderMainPage() {
+    const renderMainPage = () => {
         return (
             <>
                 <Switch>
@@ -57,22 +29,48 @@ class AdminPanel extends Component {
                 </Switch>
             </>
         )
+    };
+
+    useEffect(() => {
+        props.toggleNavbar(false);
+        props.toggleFooter(false);
+        onSelect(selected);
+        return () => {
+            props.toggleNavbar(true);
+            props.toggleFooter(true);
+        };
+    }, [props]);
+
+    const onSelect = (selected) => {
+        const { history } = props;
+        setSelected(selected);
+        const to = '/admin/' + selected;
+        if (history.location.pathname !== to) {
+            history.push(to);
+        }
+    };
+
+    const onToggle = (expanded) => {
+        setExpanded(expanded);
+    };
+
+    const getCurrentYear = () => {
+        return new Date().getFullYear();
     }
-    render() {
-        const { expanded, selected } = this.state;
-        return (
-            <div>
+
+    return(
+        <div>
                 <AdminNavbar/>
                 <SideNav
-                    className="dimpi"
-                    onSelect={this.onSelect} onToggle={this.onToggle}>
+                    className="side-navbar-custom"
+                    onSelect={onSelect} onToggle={onToggle}>
                     <SideNav.Toggle/>
                     <SideNav.Nav selected={selected}>
                         <NavItem eventKey="dashboard">
                             <NavIcon>
                                 <img src={barChartIcon} alt="bar" />
                             </NavIcon>
-                            <NavText style={{ paddingRight: 32 }} title="Dashboard">
+                            <NavText className="side-navbar-option" title="Dashboard">
                                 Dashboard
                             </NavText>
                         </NavItem>
@@ -80,7 +78,7 @@ class AdminPanel extends Component {
                             <NavIcon>
                                 <img src={userIcon} alt="user" />
                             </NavIcon>
-                            <NavText style={{ paddingRight: 32 }} title="User Manager">
+                            <NavText className="side-navbar-option" title="User Manager">
                                 User Manager
                             </NavText>
                         </NavItem>
@@ -88,7 +86,7 @@ class AdminPanel extends Component {
                             <NavIcon>
                                 <img src={newsIcon} alt="news" />
                             </NavIcon>
-                            <NavText style={{ paddingRight: 32 }} title="News Manager">
+                            <NavText className="side-navbar-option" title="News Manager">
                                 News Manager
                             </NavText>
                         </NavItem>
@@ -96,7 +94,7 @@ class AdminPanel extends Component {
                             <NavIcon>
                                 <img src={formIcon} alt="form" />
                             </NavIcon>
-                            <NavText style={{ paddingRight: 32 }} title="Form Manager">
+                            <NavText className="side-navbar-option" title="Form Manager">
                                 Form Manager
                             </NavText>
                         </NavItem>
@@ -104,7 +102,7 @@ class AdminPanel extends Component {
                             <NavIcon>
                                 <img src={feedbackIcon} alt="feedback" />
                             </NavIcon>
-                            <NavText style={{ paddingRight: 32 }} title="Feedback Manager">
+                            <NavText className="side-navbar-option" title="Feedback Manager">
                                 Feedback Manager
                             </NavText>
                         </NavItem>
@@ -112,7 +110,7 @@ class AdminPanel extends Component {
                             <NavIcon>
                                 <img src={socialMediaIcon} alt="feedback" />
                             </NavIcon>
-                            <NavText style={{ paddingRight: 32 }} title="Social Media Manager">
+                            <NavText className="side-navbar-option" title="Social Media Manager">
                                 Social Media Manager
                             </NavText>
                         </NavItem>
@@ -120,17 +118,21 @@ class AdminPanel extends Component {
                             <NavIcon>
                                 <img src={forumIcon} alt="forum" />
                             </NavIcon>
-                            <NavText style={{ paddingRight: 32 }}>
+                            <NavText className="side-navbar-option">
                                 Forum Manager
                             </NavText>
                         </NavItem>
                     </SideNav.Nav>
                 </SideNav>
                 <Main expanded={expanded}>
-                    {this.renderMainPage()}
+                    {renderMainPage()}
                 </Main>
+                <div className="copyright-admin-footer"><BiCopyright/>&nbsp;
+                {getCurrentYear()} 
+                Copyright reserved JJC Student Association USA</div>
             </div>
-        )
-    }
+    )
+
 }
+
 export default withRouter(AdminPanel);
