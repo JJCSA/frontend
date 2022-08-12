da8278b697c1ab655444f75ef044c7ac136d9c8d
Nishit Gopani <nishitgopani@gmail.com>
2022-08-11 22:56:22 -0400

    Fixed routes for admin panel

--- src/pages/admin/AdminPanel.js
+++ src/pages/admin/AdminPanel.js
@@ -1,12 +1,14 @@
-import React, { Component, useState, useEffect } from 'react';
-import AdminHomepage from './AdminHomepage';
-import styled from 'styled-components';
-import UserManager from './UserManager';
-import { barChartIcon, userIcon, newsIcon, formIcon, feedbackIcon, socialMediaIcon, forumIcon } from "../../assets/index.js";
-import { Routes as Switch, Route, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BiCopyright } from 'react-icons/bi';
 import SideNav, { NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import styled from 'styled-components';
+import { Routes as Switch, Route, useNavigate, Outlet } from 'react-router-dom';
+import AdminHomepage from './AdminHomepage';
+import UserManager from './UserManager';
+import {
+  barChartIcon, userIcon, newsIcon, formIcon, feedbackIcon, socialMediaIcon, forumIcon
+} from '../../assets/index';
 import AdminNavbar from './AdminNavbar';
-import {BiCopyright} from 'react-icons/bi';
 import './AdminPanel.scss';
 
 const Main = styled.main`
@@ -17,120 +19,116 @@ const Main = styled.main`
     `;
 
 function AdminPanel(props) {
-    const navigate = useNavigate();
-    const [selected, setSelected] = useState('dashboard');
-    const [expanded, setExpanded] = useState(false);
+  const navigate = useNavigate();
+  const [selected, setSelected] = useState('dashboard');
+  const [expanded, setExpanded] = useState(false);
 
-    const renderMainPage = () => {
-        return (
-            <>
-                <Switch>
-                    <Route exact path="/admin/dashboard" component={AdminHomepage} />
-                    <Route path="/admin/manageUsers" component={UserManager} />
-                </Switch>
-            </>
-        )
+  const onSelect = (selected) => {
+    //const { history } = props;
+    setSelected(selected);
+    const to = '/admin/' + selected;
+    navigate(to);
+    // if (history.location.pathname !== to) {
+    //   history.push(to);
+    // }
+  };
+
+  useEffect(() => {
+    props.toggleNavbar(false);
+    props.toggleFooter(false);
+    onSelect(selected);
+    return () => {
+      props.toggleNavbar(true);
+      props.toggleFooter(true);
     };
+  }, [props]);
 
-    useEffect(() => {
-        props.toggleNavbar(false);
-        props.toggleFooter(false);
-        onSelect(selected);
-        return () => {
-            props.toggleNavbar(true);
-            props.toggleFooter(true);
-        };
-    }, [props]);
 
-    const onSelect = (selected) => {
-        setSelected(selected);
-        const to = '/admin/' + selected;
-        navigate(to);
-    };
+  const onToggle = (expanded) => {
+    setExpanded(expanded);
+  };
 
-    const onToggle = (expanded) => {
-        setExpanded(expanded);
-    };
+  const getCurrentYear = () => {
+    return new Date().getFullYear();
+  };
 
-    const getCurrentYear = () => {
-        return new Date().getFullYear();
-    }
-
-    return(
-        <div>
-                <AdminNavbar/>
-                <SideNav
-                    className="side-navbar-custom"
-                    onSelect={onSelect} onToggle={onToggle}>
-                    <SideNav.Toggle/>
-                    <SideNav.Nav selected={selected}>
-                        <NavItem eventKey="dashboard">
-                            <NavIcon>
-                                <img src={barChartIcon} alt="bar" />
-                            </NavIcon>
-                            <NavText className="side-navbar-option" title="Dashboard">
-                                Dashboard
-                            </NavText>
-                        </NavItem>
-                        <NavItem eventKey="manageUsers">
-                            <NavIcon>
-                                <img src={userIcon} alt="user" />
-                            </NavIcon>
-                            <NavText className="side-navbar-option" title="User Manager">
-                                User Manager
-                            </NavText>
-                        </NavItem>
-                        <NavItem eventKey="manageNews">
-                            <NavIcon>
-                                <img src={newsIcon} alt="news" />
-                            </NavIcon>
-                            <NavText className="side-navbar-option" title="News Manager">
-                                News Manager
-                            </NavText>
-                        </NavItem>
-                        <NavItem eventKey="manageForms">
-                            <NavIcon>
-                                <img src={formIcon} alt="form" />
-                            </NavIcon>
-                            <NavText className="side-navbar-option" title="Form Manager">
-                                Form Manager
-                            </NavText>
-                        </NavItem>
-                        <NavItem eventKey="feedbackManager">
-                            <NavIcon>
-                                <img src={feedbackIcon} alt="feedback" />
-                            </NavIcon>
-                            <NavText className="side-navbar-option" title="Feedback Manager">
-                                Feedback Manager
-                            </NavText>
-                        </NavItem>
-                        <NavItem eventKey="socialMediaManager">
-                            <NavIcon>
-                                <img src={socialMediaIcon} alt="feedback" />
-                            </NavIcon>
-                            <NavText className="side-navbar-option" title="Social Media Manager">
-                                Social Media Manager
-                            </NavText>
-                        </NavItem>
-                        <NavItem eventKey="forumManager">
-                            <NavIcon>
-                                <img src={forumIcon} alt="forum" />
-                            </NavIcon>
-                            <NavText className="side-navbar-option">
-                                Forum Manager
-                            </NavText>
-                        </NavItem>
-                    </SideNav.Nav>
-                </SideNav>
-                <Main expanded={expanded}>
-                    {renderMainPage()}
-                </Main>
-                <div className="copyright-admin-footer"><BiCopyright/>&nbsp;
-                {getCurrentYear()} 
-                Copyright reserved JJC Student Association USA</div>
-            </div>
-    )
-
+  return(
+    <div>
+      <AdminNavbar />
+      <SideNav
+        className="side-navbar-custom"
+        onSelect={onSelect} onToggle={onToggle}>
+        <SideNav.Toggle />
+        <SideNav.Nav selected={selected}>
+          <NavItem eventKey="dashboard">
+            <NavIcon>
+              <img src={barChartIcon} alt="bar" />
+            </NavIcon>
+            <NavText className="side-navbar-option" title="Dashboard">
+              Dashboard
+            </NavText>
+          </NavItem>
+          <NavItem eventKey="manageUsers">
+            <NavIcon>
+              <img src={userIcon} alt="user" />
+            </NavIcon>
+            <NavText className="side-navbar-option" title="User Manager">
+              User Manager
+            </NavText>
+          </NavItem>
+          <NavItem eventKey="manageNews">
+            <NavIcon>
+              <img src={newsIcon} alt="news" />
+            </NavIcon>
+            <NavText className="side-navbar-option" title="News Manager">
+              News Manager
+            </NavText>
+          </NavItem>
+          <NavItem eventKey="manageForms">
+            <NavIcon>
+              <img src={formIcon} alt="form" />
+            </NavIcon>
+            <NavText className="side-navbar-option" title="Form Manager">
+              Form Manager
+            </NavText>
+          </NavItem>
+          <NavItem eventKey="feedbackManager">
+            <NavIcon>
+              <img src={feedbackIcon} alt="feedback" />
+            </NavIcon>
+            <NavText className="side-navbar-option" title="Feedback Manager">
+              Feedback Manager
+            </NavText>
+          </NavItem>
+          <NavItem eventKey="socialMediaManager">
+            <NavIcon>
+              <img src={socialMediaIcon} alt="feedback" />
+            </NavIcon>
+            <NavText className="side-navbar-option" title="Social Media Manager">
+              Social Media Manager
+            </NavText>
+          </NavItem>
+          <NavItem eventKey="forumManager">
+            <NavIcon>
+              <img src={forumIcon} alt="forum" />
+            </NavIcon>
+            <NavText className="side-navbar-option">
+              Forum Manager
+            </NavText>
+          </NavItem>
+        </SideNav.Nav>
+      </SideNav>
+      <Main expanded={expanded}>
+        <Outlet />
+      </Main>
+      <div className="copyright-admin-footer">
+        <BiCopyright />
+        &nbsp;
+        {getCurrentYear()}
+        Copyright reserved JJC Student Association USA
+      </div>
+    </div>
+  );
 }
 
 export default AdminPanel;
