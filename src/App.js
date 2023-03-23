e2dbb522101448fe608116fb1353304de04b31d1
Deshna Shah <55919301+deshna-s@users.noreply.github.com>
2023-03-23 09:40:05 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,34 +6,26 @@ import './App.scss';
 import { AuthProvider } from 'react-auth-kit';
 import refreshApi from './helpers/refreshApi';
 import GlobalContext from './store/GlobalContext';
-import Routes from './Routes'; 
-<h1>React Responsive - a guide</h1>
-
-//Reponsive
+import Routes from './Routes';
 import { useMediaQuery } from "react-responsive";
-
-// Components
 import { Desktop } from "./components/desktop/desktop.component";
 import { Laptop } from "./components/laptop/laptop.component";
 import { BigScreen } from "../src/components/big-screen/big-screen.component";
 import { Mobile } from "../src/components/mobile/mobile.component";
 import { TabletMobile } from "../src/components/tablet-mobile/tablet-mobile.component";
 
-//responsive
 
 function App() {
   const [globalState, setGlobalState] = useState({ profile: null });
-
-  // Start Responsive Webiste
-
-  const isMobileDevice = useMediaQuery({
+  
+    const isMobileDevice = useMediaQuery({
     query: "(min-device-width: 480px)",
   });
-
+												
   const isTabletDevice = useMediaQuery({
     query: "(min-device-width: 768px)",
   });
-
+									
   const isLaptop = useMediaQuery({
     query: "(min-device-width: 1024px)",
   });
@@ -41,15 +33,13 @@ function App() {
   const isDesktop = useMediaQuery({
     query: "(min-device-width: 1200px)",
   });
-
+												
   const isBigScreen = useMediaQuery({
     query: "(min-device-width: 1201px )",
   });
 
-  // end of responsive website
-
   return (
-
+  
     <><div className='App'>
       <h1>React Responsive - a guide</h1>
       {isMobileDevice && <Mobile />}
@@ -61,17 +51,18 @@ function App() {
       </>}
 
     </div>
+	
     <GlobalContext.Provider value={{ globalState, setGlobalState }}>
-        <AuthProvider
-          authType="cookie"
-          authName="_auth"
-          cookieDomain={window.location.hostname}
-          cookieSecure={window.location.protocol === 'https:'}
-          refresh={refreshApi}
-        >
-          <Routes />
-        </AuthProvider>
-      </GlobalContext.Provider></>
+      <AuthProvider
+        authType="cookie"
+        authName="_auth"
+        cookieDomain={window.location.hostname}
+        cookieSecure={window.location.protocol === 'https:'}
+        refresh={refreshApi}
+      >
+        <Routes />
+      </AuthProvider>
+    </GlobalContext.Provider>
   );
 }
 
