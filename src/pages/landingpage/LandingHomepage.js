078d8f8f9997a9e34ec3d4cd6df90680cacd20a2
psheth6 <psheth6@hawk.iit.edu>
2023-04-23 19:52:36 -0500

    MissionVission Page

--- src/pages/landingpage/LandingHomepage.js
+++ src/pages/landingpage/LandingHomepage.js
@@ -2,12 +2,14 @@ import React, {useEffect} from 'react';
 import ContactUs from './components/contactUs/ContactUs';
 import NewsFeed from './components/newsFeed/NewsFeed';
 import JJCMission from './components/jjcMission/JJCMission';
+import MissionVision from './components/missionVision/MissionVision';
 
 function LandingHomepage() {
 
   return (
     <div>
       <JJCMission />
+      <MissionVision />
       <NewsFeed />
       <ContactUs />
     </div>
