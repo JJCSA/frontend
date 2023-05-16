e6683d4a9ddb97e4acb3704ef13cfe97fd578527
Nishit Gopani <nishitgopani@gmail.com>
2023-05-15 23:54:56 -0400

    Added component for testimonial

--- src/pages/landingpage/LandingHomepage.js
+++ src/pages/landingpage/LandingHomepage.js
@@ -1,14 +1,15 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import ContactUs from './components/contactUs/ContactUs';
 import NewsFeed from './components/newsFeed/NewsFeed';
 import JJCMission from './components/jjcMission/JJCMission';
+import Testimonial from './components/testimonial/Testimonial';
 
 function LandingHomepage() {
-
   return (
     <div>
       <JJCMission />
       <NewsFeed />
+      <Testimonial />
       <ContactUs />
     </div>
   );
