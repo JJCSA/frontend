30c68f2ee60a4320ef43fd0e64f6dcf6a9d4b705
Nishit Gopani <nishitgopani@gmail.com>
2023-05-18 01:46:29 -0400

    Initial commit for testimonial

--- src/pages/landingpage/components/testimonial/Testimonial.js
+++ src/pages/landingpage/components/testimonial/Testimonial.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import './Testimonial.scss';
 
 function Testimonial() {
   const settings = {
@@ -12,36 +13,42 @@ function Testimonial() {
     slidesToScroll: 3,
   };
   return (
-    <div className="container">
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
+    <div id="testimonial" className="TESTIMONIAL">
+      <div className="container">
+        <div className="text-center mt-5">
+          <h4>Testimonial</h4>
+          <hr className="testimonial" />
         </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-        <div>
-          <h3>7</h3>
-        </div>
-        <div>
-          <h3>8</h3>
-        </div>
-        <div>
-          <h3>9</h3>
-        </div>
-      </Slider>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+          <div>
+            <h3>7</h3>
+          </div>
+          <div>
+            <h3>8</h3>
+          </div>
+          <div>
+            <h3>9</h3>
+          </div>
+        </Slider>
+      </div>
     </div>
   );
 }
