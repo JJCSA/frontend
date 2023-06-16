2094bf90fec26a1c0a7cbb8191f47de32a954616
Nishit Gopani <nishitgopani@gmail.com>
2023-06-15 23:14:05 -0400

    Fixed aboutjs and jjcmission

--- src/pages/landingpage/components/aboutUs/AboutUs.js
+++ src/pages/landingpage/components/aboutUs/AboutUs.js
@@ -3,40 +3,52 @@ import './AboutUs.scss';
 
 const AboutUs = () => {
   return (
-    
-    <div className="container">
-      <div className='aboutUsHeading'>
-        <h4>About Us</h4>
+    <div id="about-us" className="ABOUTUS">
+      <div className="text-center mt-5">
+        <h4>ABOUT US</h4>
         <hr className="about-us" />
       </div>
-      <div className="row">
-        <div className="col-md-6">
-          <div className='aboutUsImage'>
-
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="aboutUsImage" />
           </div>
-        </div>
-        <div className="col-md-6">
-          <div>
-            <p className='content'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin accumsan risus et porta sagittis.
-              Duis at tortor vulputate nunc posuere luctus. Curabitur massa neque, dignissim nec turpis et, tincidunt
-              mollis dolor. Sed aliquam velit ac elit commodo tincidunt. Suspendisse egestas erat quam, id
-            </p>
-            <div className='content1'>
-              <p>
-                “Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s”
-              </p>
+          <div className="col-md-6">
+            <div className="about-us-text">
+              <blockquote>
+                JJC USA is a non profit organization, a subsidiary of Jain
+                Jagruti Centre Central Board and Charitable trust. It was formed
+                in 2019 with a motive to assist Jain community members living in
+                the United States of America. The organization is formed by a
+                diverse group of volunteers working together in different areas
+                to deliver initiatives for incoming students and working
+                professionals who are already settled in the US. In addition, we
+                are committed to improve and provide a platform to Jain students
+                and working professionals by organizing social and professional
+                events to build a community of like minded individuals.
+              </blockquote>
             </div>
-            <div className='knowMoreBox'>
-              <button type="button" href="/AboutUsContent" className='knowMore'>Know More</button>
+            <div className="knowMoreBox">
+              <button
+                type="button"
+                href="/AboutUsContent"
+                className="btn btn-light mx-auto d-flex justify-content-center know-more-button"
+              >
+                Know More
+              </button>
             </div>
           </div>
         </div>
       </div>
     </div>
+    // <div className="fluid-container">
+    //   <div className="aboutUsHeading">
+    //     <h4>About Us</h4>
+    //     <hr className="about-us" />
+    //   </div>
 
+    // </div>
   );
-}
+};
 
 export default AboutUs;
