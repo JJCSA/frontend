5eb4b57c3042a212de06fcf2349c460d7ddc4818
psheth6 <psheth6@hawk.iit.edu>
2023-06-19 19:21:39 -0500

    reset password

--- src/components/forgotPassword/ForgotPassword.js
+++ src/components/forgotPassword/ForgotPassword.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './ForgotPassword.scss';
-<<<<<<< HEAD
 import { Form, Col, Row, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
@@ -32,43 +31,6 @@ const ForgotPassword = () => (
                   name="username"
                 />
               </Form.Group>
-=======
-import { Form, Col, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-
-const ForgotPassword = () => (
-  <Row className="FORGOT-PASSWORD">
-    <Col>
-      <div className="forgot-password-container">
-        <div className="forgot-password-header">
-          <h4>Forgot Password?</h4>
-        </div>
-        <div className="forgot-password-content">
-          <p>
-            Enter the email address you used to create the account. We will
-            email you the instructions to reset your password.
-          </p>
-        </div>
-        {/* outline box layout */}
-        <div className="outlinebox">
-          {/* Email Label */}
-          <Form className="label">
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>
-                Email
-                <span style={{ color: 'red' }}>*</span>
-              </Form.Label>
-              {/* inline box layout */}
-              <Form.Control
-                type="email"
-                placeholder=""
-                className="inlinebox"
-                name="username"
-              />
-            </Form.Group>
-            <div>
-              {/* button box */}
->>>>>>> 8c01972 (Fixed issue (#129))
               <div className="button-box">
                 <Link to="/ForgotPasswordMessage">
                   <button type="submit" className="submit-button">
@@ -83,8 +45,4 @@ const ForgotPassword = () => (
     </Row>
   </Container>
 );
-<<<<<<< HEAD
-
-=======
->>>>>>> 8c01972 (Fixed issue (#129))
 export default ForgotPassword;
