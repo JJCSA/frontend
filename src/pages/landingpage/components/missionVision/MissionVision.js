74e1867f620d6780651e9fd0948f585008ce1d27
Nishit Gopani <nishitgopani@gmail.com>
2023-07-19 03:29:51 -0400

    About us new (#149)
    * About us
    
    * Volunteers and New Mission Vision
    
    * Removing mission and vision from navbar
    
    * Fixed timeline issue and updated colors and design as per landing page theme
    
    * New About Us
    
    * Routes Error fixed
    
    * Footer changes
    
    * Updated footer
    
    ---------
    
    Co-authored-by: Visesh <viseshjain.99@gmail.com>

--- src/pages/landingpage/components/missionVision/MissionVision.js
+++ src/pages/landingpage/components/missionVision/MissionVision.js
@@ -1,58 +1,66 @@
 import React from 'react';
 import './MissionVision.scss';
 
-
-const MissionVision = () => {
+const NewMissionVision = () => {
   return (
-    //main mission & Vision
-    <div id="mission-vision" className="Mission-Vision"> 
+    <div id="mission-vision" className="MISSIONVISION">
+      <div className="text-center mt-5">
+        <h4>MISSION AND VISION</h4>
+        <hr className="mission-vision" />
+      </div>
 
-        {/* frame 3 */}
-        <div className='missionvissionimage'> 
-        {/* <h4 className='missionvissionheader'>
-            Vision and Mission
-        </h4>   */}
-        </div>
-
-        {/* frame 1 */}
-        <div className='visionframe'>
-            <h4 className='Vision'> 
-                Our Vision 
-            </h4>
-            <p className='visioncontent'>
-                To be an inclusive Jain organisation in the United States that provides support, guidance, and valuable resources to students and alumni.
+      <div className="container mt-5">
+        <div className="card mb-4">
+          <div className="card-body">
+            <h4 className="card-title vision-title">Our Vision</h4>
+            <p className="card-text">
+              To be an inclusive Jain organisation in the United States that
+              provides support, guidance, and valuable resources to students and
+              alumni.
             </p>
+          </div>
         </div>
 
-        {/* frame 2 */}
-        <div className='missionframe'>
-            <h4 className='Mission'>
-                Our Mission
-            </h4>
-            <p className='missioncontent'>
-            Our mission is to provide a platform to facilitate community engagement amongst Jain students and alumni across the United 
-            States. We help members attain professional growth by organising events and interactive Q and A’s. We also enable social 
-            support systems to navigate through transitions in the USA.
+        <div className="card mb-4">
+          <div className="card-body">
+            <h4 className="card-title vision-title">Our Mission</h4>
+            <p className="card-text">
+              Our mission is to provide a platform to facilitate community
+              engagement amongst Jain students and alumni across the United
+              States. We help members attain professional growth by organizing
+              events and interactive Q and A’s. We also enable social support
+              systems to navigate through transitions in the USA.
             </p>
+          </div>
         </div>
 
-        {/* externalobjective frame 1 */}
-        <div className='externalobjectiveframe'>
-            <h4 className='externalobjective'>
-                External Objectives
-            </h4>
-            <p className='externalobjectivecontent'>
-            <ol>
-            <li>Provide opportunities to build social connections through local meet-ups, virtual events, and social media groups.</li>                                                    
-            <li>Create opportunities for professional growth and career development through events like “alumni symposiums” and “tips to ace your interview” etc.</li>
-            <li>Ease transitions for new students coming to the United States of America by facilitating programs like buddy project</li>
-            <li>Strengthen the community by creating associations with fellow Jain organisations.</li>
+        <div className="card">
+          <div className="card-body">
+            <h4 className="card-title vision-title">External Objectives</h4>
+            <ol className="card-text">
+              <li>
+                Provide opportunities to build social connections through local
+                meet-ups, virtual events, and social media groups.
+              </li>
+              <li>
+                Create opportunities for professional growth and career
+                development through events like "alumni symposiums" and "tips to
+                ace your interview," etc.
+              </li>
+              <li>
+                Ease transitions for new students coming to the United States of
+                America by facilitating programs like the buddy project.
+              </li>
+              <li>
+                Strengthen the community by creating associations with fellow
+                Jain organizations.
+              </li>
             </ol>
-            </p>
+          </div>
         </div>
-
+      </div>
     </div>
   );
-}
+};
 
-export default MissionVision;
+export default NewMissionVision;
